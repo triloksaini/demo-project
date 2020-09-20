c2b21a27708dc5a3930a0062f538469fdee80418
triloksaini <triloksaini07@gmail.com>
2020-09-20 20:36:38 +0530

    Commit

---
+++ MavenProject-master/MavenProject-master/server/src/main/java/com/example/Greeter.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+  //to-d0: Add javadoc comment
+  public final String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
